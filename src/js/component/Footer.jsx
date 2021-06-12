fa1f4787331f0d8df27602e473c7b8649a5c4509
Bryan García <bryan1997st@gmail.com>
2021-06-11 20:10:51 -0500

    Se creó el componente footer y dio estilo al mismo.

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -1,10 +1,77 @@
 import React, { Component } from "react";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+export const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="footer__section">
+				<ul>
+					<li className="footer__section--li">
+						<h2>Servicio al cliente</h2>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Preguntas frecuentes
+						</a>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Términos y condiciones
+						</a>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Privacidad y Seguridad
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="footer__section">
+				<ul>
+					<li className="footer__section--li">
+						<h2>Para empresas</h2>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Registra tu negocio
+						</a>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Inicia sesión como empresa
+						</a>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Preguntas frecuentes
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="footer__section">
+				<ul>
+					<li className="footer__section--li">
+						<h2>Sobre Nosotros</h2>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Nosotros
+						</a>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Contáctanos
+						</a>
+					</li>
+					<li className="footer__section--li">
+						<a rel="" href="#">
+							Vende con nosotros
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="footer__copyr">
+				<span>Copy Right © 2021 </span>
+			</div>
+		</footer>
+	);
+};

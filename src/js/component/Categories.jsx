60733f94c3f4321c17421ce9a39e0ceeea0d2a79
Bryan García <bryan1997st@gmail.com>
2021-06-12 22:35:03 -0500

    Se creó el componente y se le dio estilos al mismo

--- src/js/component/Categories.jsx
+++ src/js/component/Categories.jsx
@@ -5,10 +5,13 @@ export const Categories = () => {
 	const colors = ["#DBFA6E", "#FA7261", "#87FAC0", "#C587E4", "#43db76", "#DBFA6E"];
 	const categories = ["Frutas", "Vegetales", "Tiendas", "Dulces", "Licores", "Ubicación"];
 	return (
-		<div className="categories" id="categories">
-			{categories.map((element, index) => {
-				return <Category color={colors[index]} category={element} key={index} />;
-			})}
+		<div className="container">
+			<h2>Categorías</h2>
+			<div className="categories" id="categories">
+				{categories.map((element, index) => {
+					return <Category color={colors[index]} category={element} key={index} />;
+				})}
+			</div>
 		</div>
 	);
 };

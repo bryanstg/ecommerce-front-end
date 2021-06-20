1e860599665b98d3ec6165805d9442c769133a59
Bryan García <bryan1997st@gmail.com>
2021-06-20 13:45:40 -0500

    Se importó el componente Products

--- src/js/views/BuyerStore.jsx
+++ src/js/views/BuyerStore.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { Products } from "../component/Products.jsx";
 
 export const BuyerStore = () => {
 	const { store, actions } = useContext(Context);
@@ -27,25 +28,28 @@ export const BuyerStore = () => {
 		}
 	});
 
-	const outStock = store.buyer.storeData.products.filter(product => (product.amount_available = 0));
+	const outStock = store.buyer.storeData.products.filter(product => product.amount_available === 0);
 	return (
 		<div className="store-container">
 			{store.buyer.storeData.info.id ? (
 				<div className="store">
 					<h2 className="store__title">{store.buyer.storeData.info.name}</h2>
 					<div className="store__info">
-						<a href="" id="" className="store__info--box">
-							{`Productos: ${store.buyer.storeData.info.id_products.quantity}`}
+						<a href="" className="store__info--box">
+							{`Total`}
+							<span id="total-box">{store.buyer.storeData.info.id_products.quantity}</span>
 						</a>
 						<a href="#active" className="store__info--box">
-							{`Productos activos: ${activeProducts.length}`}
+							{`Activos`} <span id="active-box">{activeProducts.length}</span>
 						</a>
-						<a href="#inactive" id="inactivos" className="store__info--box">{`Productos inactivos: ${
-							inactiveProducts.length
-						}`}</a>
-						<a href="#out-stock" className="store__info--box">{`Productos sin stock: ${
-							outStock.length
-						}`}</a>
+						<a href="#inactive" className="store__info--box">
+							{`Inactivos`}
+							<span id="inactive-box">{inactiveProducts.length}</span>
+						</a>
+						<a href="#out-stock" className="store__info--box">
+							{`Sin stock`}
+							<span id="out-stock-box">{outStock.length}</span>
+						</a>
 					</div>
 					<div id="" className="store__add-product">
 						<h3 className="store__add-product--title">Agregar producto</h3>
@@ -58,9 +62,15 @@ export const BuyerStore = () => {
 							<i className="far fa-plus-square fa-2x" />
 						</button>
 					</div>
-					<div id="active" className="store__active">{`active`}</div>
-					<div id="inactive" className="store__inactive">{`inactive`}</div>
-					<div id="out-stock" className="store__out-stock">{`out-stock`}</div>
+					<div id="active" className="store__active">
+						<Products type="activos" />
+					</div>
+					<div id="inactive" className="store__inactive">
+						<Products type="inactivos" />
+					</div>
+					<div id="out-stock" className="store__out-stock">
+						<Products type="sin stock" />
+					</div>
 				</div>
 			) : (
 				<div className="spinner-border text-success" role="status">

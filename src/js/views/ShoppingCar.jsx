a3f387bcfd516ca5935f2b24425a6628b4208f4d
Bryan García <bryan1997st@gmail.com>
2021-06-24 03:11:46 -0500

    Se creo el carrito de compras y se estilizó el mismo

--- src/js/views/ShoppingCar.jsx
+++ src/js/views/ShoppingCar.jsx
@@ -1,18 +1,33 @@
 import React, { useContext, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { Context } from "../store/appContext";
+import shoppingCar from "./../../img/cart.png";
+import { ProductToBuy } from "../component/ProductToBuy.jsx";
 
 export const ShoppingCar = () => {
 	const { store, actions } = useContext(Context);
-
-	useEffect(() => {
-		actions.getShoppingCar(1);
-	}, []);
+	if (store.user.token && store.user.role == "buyer") {
+		useEffect(() => {
+			actions.getShoppingCar(store.user.info.user_buyer.id);
+		}, []);
+	}
 	return (
 		<React.Fragment>
 			{store.user.token && store.user.role == "buyer" ? (
-				<div className="container">
-					<h3>Este es el carrito</h3>
+				<div className="car-container">
+					<div className="car__title">
+						<h3>
+							Carrito de
+							<br />
+							<span>Compras</span>
+						</h3>
+						<img src={shoppingCar} alt="" width="120" />
+					</div>
+					<div className="car__store">
+						{store.buyer.shoppingCar.map(productToBuy => {
+							return <ProductToBuy key={productToBuy.id} productToBuy={productToBuy} />;
+						})}
+					</div>
 				</div>
 			) : (
 				<Redirect to="/login" />

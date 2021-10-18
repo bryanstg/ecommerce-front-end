83284ed39f305a5972fb5b81d23dbc7a83676c23
Bryan García <bryan1997st@gmail.com>
2021-10-18 15:52:37 -0500

    Added categories description and images

--- src/js/component/Categories.jsx
+++ src/js/component/Categories.jsx
@@ -1,9 +1,46 @@
 import React from "react";
 import { Category } from "./Category.jsx";
+import Vegetales from "./../../img/svg/broccoli.png";
+import Frutas from "./../../img/svg/manzana.png";
+import Tiendas from "./../../img/svg/tiendas.png";
+import Dulces from "./../../img/svg/dulce.png";
+import Licores from "./../../img/svg/licor.png";
+import Ubicación from "./../../img/svg/ubicacion.png";
 
 export const Categories = () => {
 	const colors = ["#DBFA6E", "#FA7261", "#87FAC0", "#C587E4", "#43db76", "#DBFA6E"];
-	const categories = ["Frutas", "Vegetales", "Tiendas", "Dulces", "Licores", "Ubicación"];
+	const categories = [
+		{
+			category: "Frutas",
+			url: Frutas,
+			info: "Consigue una inmesa variedad de frutas y apoya el mercado local."
+		},
+		{
+			category: "Vegetales",
+			url: Vegetales,
+			info: "Consígue los más frescos y de alta calidad de la cuadra."
+		},
+		{
+			category: "Tiendas",
+			url: Tiendas,
+			info: "Tu decides en que tienda comprar. Realiza tu busqueda por tiendas también."
+		},
+		{
+			category: "Dulces",
+			url: Dulces,
+			info: "¿Noche de películas? Consigue los dulces que necesites y recíbelos en la comodida de tu hogar."
+		},
+		{
+			category: "Licores",
+			url: Licores,
+			info: "¿De fiesta? Consigue todo lo que necesitas con nosotros."
+		},
+		{
+			category: "Ubicación",
+			url: Ubicación,
+			info: "Encuentra la tienda que mejor se acomode a tu ubicación. Entregas más rápidas, clientes más felices."
+		}
+	];
 	return (
 		<div className="container">
 			<h2>Categorías</h2>
--- src/js/component/Category.jsx
+++ src/js/component/Category.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import Vegetales from "./../../img/svg/broccoli.png";
-import Frutas from "./../../img/svg/manzana.png";
-import Tiendas from "./../../img/svg/tiendas.png";
-import Dulces from "./../../img/svg/dulce.png";
-import Licores from "./../../img/svg/licor.png";
-import Ubicación from "./../../img/svg/ubicacion.png";
 
 import PropTypes from "prop-types";
-const img = [Vegetales, Frutas, Tiendas, Dulces, Ubicación, Licores];
 
 export const Category = props => {
+	console.log(typeof props.category, props.category);
 	return (
 		<div
 			className={`
@@ -37,19 +31,17 @@ export const Category = props => {
                         
                     `}
 					style={{ backgroundColor: props.color }}>
-					<img src={Tiendas} alt="" width="60" />
+					<img src={props.category.url} alt="" width="60" />
 				</div>
 			</div>
 			<div className={`category__info`}>
-				<h3 className="category__info--title">{`${props.category}`}</h3>
-				<p className="category__info--paragraph">
-					{` Lorem ipsum dolor sit, amet consectetur adipisicing elit.`}
-				</p>
+				<h3 className="category__info--title">{`${props.category.category}`}</h3>
+				<p className="category__info--paragraph">{props.category.info}</p>
 			</div>
 		</div>
 	);
 };
 Category.propTypes = {
 	color: PropTypes.string,
-	category: PropTypes.string
+	category: PropTypes.object
 };
